5d42a5e8c78ee141b94ebfe9239d04ae20d8cdeb
leo-bimpf <lkirilov86@gmail.com>
2026-05-29 12:09:10 +0300

    feat: added allure and logging integration

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,9 +1,20 @@
 from clients.pet_client import PetClient
+from utils.allure_utils import log_response
+import allure
 pet_client = PetClient()
 
+@allure.feature("Pet API")
+@allure.story("Create pet")
+@allure.title("Create pet with valid payload")
 def test_create_pet(pet_payload):
     response = pet_client.create_pet(pet_payload)
-    print("\nCREATE RESPONSE:", response.json())
+    allure.attach(
+        str(response.request.url),
+        name="Request URL",
+        attachment_type=allure.attachment_type.TEXT
+    )
+
+    log_response(response)
 
     assert response.status_code == 200
 
@@ -12,13 +23,21 @@ def test_create_pet(pet_payload):
     assert data["name"] == pet_payload["name"]
     assert data["status"] == pet_payload["status"]
 
-
+@allure.feature("Pet API")
+@allure.story("Delete pet")
+@allure.title("Delete pet and verify it is removed")
 def test_delete_pet(pet_payload):
     create_response = pet_client.create_pet(pet_payload)
     pet_id = create_response.json()["id"]
 
     delete_response = pet_client.delete_pet(pet_id)
-    print("\nDELETE RESPONSE:", delete_response.json())
+    allure.attach(
+        str(delete_response.request.url),
+        name="DELETE URL",
+        attachment_type=allure.attachment_type.TEXT
+    )
+
+    log_response(delete_response)
 
     assert delete_response.status_code == 200
 
@@ -28,12 +47,22 @@ def test_delete_pet(pet_payload):
 
     assert get_after_delete.status_code == 404
 
+@allure.feature("Pet API")
+@allure.story("Get pet")
+@allure.title("Get created pet by id")
+
 def test_get_pet(created_pet):
     payload, pet_id = created_pet
 
     response = pet_client.get_pet(pet_id)
 
-    print("\nGET RESPONSE:", response.json())
+    allure.attach(
+        str(response.request.url),
+        name="GET URL",
+        attachment_type=allure.attachment_type.TEXT
+    )
+
+    log_response(response)
 
     assert response.status_code == 200
     assert response.json()["id"] == payload["id"]
---
+++ utils/allure_utils.py
@@ -0,0 +1,14 @@
+import allure
+
+def log_response(response):
+    try:
+        body = response.json()
+    except Exception:
+        body = response.text
+
+    allure.attach(
+        str(body),
+        name="Response",
+        attachment_type=allure.attachment_type.JSON,
+    )
+
