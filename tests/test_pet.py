db5dcb88b83658b63cca5c454c77fbfcbbcd51cc
leo-bimpf <lkirilov86@gmail.com>
2026-05-29 10:58:08 +0300

    feat: added some logging

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -3,6 +3,8 @@ pet_client = PetClient()
 
 def test_create_pet(pet_payload):
     response = pet_client.create_pet(pet_payload)
+    print("\nCREATE RESPONSE:", response.json())
+
     assert response.status_code == 200
 
     data = response.json()
@@ -16,15 +18,22 @@ def test_delete_pet(pet_payload):
     pet_id = create_response.json()["id"]
 
     delete_response = pet_client.delete_pet(pet_id)
+    print("\nDELETE RESPONSE:", delete_response.json())
+
     assert delete_response.status_code == 200
 
     get_after_delete = pet_client.get_pet(pet_id)
+
+    print("\nGET AFTER DELETE:", get_after_delete.json())
+
     assert get_after_delete.status_code == 404
 
 def test_get_pet(created_pet):
     payload, pet_id = created_pet
 
     response = pet_client.get_pet(pet_id)
+
+    print("\nGET RESPONSE:", response.json())
 
     assert response.status_code == 200
     assert response.json()["id"] == payload["id"]
